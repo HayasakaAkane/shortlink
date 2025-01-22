a1ececce4c00043df2421320045b111d256515ce
ShikiNatsume <2261768355@qq.com>
2025-01-22 12:21:10 +0800

    feature:开发短链接核心项目初始化

--- admin/src/main/java/org/natsume/shortlink/admin/ShortLinkAdminApplication.java
+++ admin/src/main/java/org/natsume/shortlink/admin/ShortLinkAdminApplication.java
@@ -1,5 +1,6 @@
 package org.natsume.shortlink.admin;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -13,6 +14,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @Version 1.0
  */
 @SpringBootApplication
+@MapperScan("org.natsume.shortlink.admin.dao.mapper")
 public class ShortLinkAdminApplication {
     public static void main(String[] args) {
         SpringApplication.run(ShortLinkAdminApplication.class, args);
--- admin/src/main/java/org/natsume/shortlink/admin/controller/UserController.java
+++ admin/src/main/java/org/natsume/shortlink/admin/controller/UserController.java
@@ -1,5 +1,11 @@
 package org.natsume.shortlink.admin.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.natsume.shortlink.admin.common.convention.result.Result;
+import org.natsume.shortlink.admin.common.convention.result.Results;
+import org.natsume.shortlink.admin.common.enums.UserErrorCodeEnum;
+import org.natsume.shortlink.admin.dto.resp.UserRespDto;
+import org.natsume.shortlink.admin.service.UserService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,14 +20,22 @@ import org.springframework.web.bind.annotation.RestController;
  * @Version 1.0
  */
 @RestController
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
 
     /**
      * 根据用户名查询用户信息
      */
     @GetMapping("/api/shortlink/v1/user/{username}")
-    public String getUserByUsername(@PathVariable("username") String username) {
-        return "hi " + username;
+    public Result<UserRespDto> getUserByUsername(@PathVariable("username") String username) {
+        UserRespDto result = userService.getUserByUsername(username);
+        if(result == null) {
+            return new Result<UserRespDto>().setCode(UserErrorCodeEnum.USER_NULL.code()).setMessage(UserErrorCodeEnum.USER_NULL.message());
+        } else{
+            return Results.success(result);
+        }
     }
 
 }
