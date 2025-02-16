025d852d33ce19e741d2e21b8a53200cfbea4681
ShikiNatsume <2261768355@qq.com>
2025-02-16 23:17:55 +0800

    feature:添加不同过滤器，用于用户注册时可以不查数据库而是去查布隆过滤器，可以允许一点误差

---
+++ admin/src/main/java/org/natsume/shortlink/admin/config/RBloomFilterConfiguration.java
@@ -0,0 +1,29 @@
+package org.natsume.shortlink.admin.config;
+
+import org.redisson.api.RBloomFilter;
+import org.redisson.api.RedissonClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * ClassName: RBloomFilterConfiguration
+ * Package: org.natsume.shortlink.admin.config
+ * Description: 布隆过滤器配置
+ *
+ * @Author natsume
+ * @Create 2025/1/22 21:21
+ * @Version 1.0
+ */
+@Configuration
+public class RBloomFilterConfiguration {
+
+    /**
+     * 防止用户注册查询数据库的布隆过滤器
+     */
+    @Bean
+    public RBloomFilter<String> userRegisterCachePenetrationBloomFilter(RedissonClient redissonClient) {
+        RBloomFilter<String> cachePenetrationBloomFilter = redissonClient.getBloomFilter("userRegisterCachePenetrationBloomFilter");
+        cachePenetrationBloomFilter.tryInit(100000000L, 0.001);
+        return cachePenetrationBloomFilter;
+    }
+}
